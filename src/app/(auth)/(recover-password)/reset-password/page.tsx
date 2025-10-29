b8e724a4b359844856d9a589418c7fc04102312b
manmohan841208 <manmohan.4.singh@coforge.com>
2025-10-29 12:14:31 +0530

    updated files

--- src/app/(auth)/(recover-password)/reset-password/page.tsx
+++ src/app/(auth)/(recover-password)/reset-password/page.tsx
@@ -85,12 +85,11 @@ const ResetPasswordPage = () => {
         className="w-full bg-[var(--color-white)] !p-0 md:w-[74.65%]"
         header={showSuccessAlert ? 'MyECP Password Reset' : 'For Your Security'}
       >
-        <div className="flex flex-col p-6 !pb-0 sm:gap-4">
+        <div className="flex flex-col p-4 !pb-0 gap-4">
           {showAlert ? (
             <CustomAlert
               type="error"
               description={errorMessage}
-              className="mb-2"
             />
           ) : (
             ''
@@ -110,8 +109,8 @@ const ResetPasswordPage = () => {
 
           {showSuccessAlert ? null : (
             <div className="flex justify-end">
-              <b>
-                <span className="px-1 text-[var(--text-error)]">*</span>
+              <b className='!text-[14px]'>
+                <span className="px-1 text-[var(--text-error)] ">*</span>
                 {REQUIRED_FIELDS}
               </b>
             </div>
