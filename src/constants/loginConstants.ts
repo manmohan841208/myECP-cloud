6b3304ee076a1c6961f9e610a27fd9f1691e4df7
manmohan841208 <manmohan.4.singh@coforge.com>
2025-10-31 14:02:32 +0530

    modified cloud build filea and more

--- src/constants/loginConstants.ts
+++ src/constants/loginConstants.ts
@@ -1,6 +1,6 @@
-export const USER_ID_LABEL = 'User ID';
+export const USER_ID_LABEL = 'User ID singh manmohan';
 export const USER_PASSWORD_LABEL = 'Password';
-export const LOGIN_BTN = 'Log In';
+export const LOGIN_BTN = 'Log In --Manmohan';
 export const FORGOT = 'Forgot';
 export const FORGOT_PASSWORD = 'Password';
 export const REMEMBER_USER_ID = 'Remember User ID';
